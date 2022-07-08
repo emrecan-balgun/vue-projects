bef173fab61363f2c6fd510fc49b6f4565d9e540
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-08 16:38:27 +0300

    add filters

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="text-center mt-2">
         <h1 class="mb-2">Todo simple app</h1>
-        <!-- <input type="text" @keydown.enter="filterTodos"> -->
-          <ul v-for="todo in todoList" :key="todo.id">
+        <input type="text" v-model="todoText" @keydown.enter="addTodo(todoText)" placeholder="Add new todo item">
+          <ul v-for="todo in filteredTodos" :key="todo.id">
             <li class="d-flex justify-content-between align-items-center" :style="{ marginBottom: '10px' }">
               <span :class="todo.completed === true ? 'underline' : ''">{{ todo.text }}</span>
               <div class="d-flex" :style="{ gap: '10px' }">
@@ -12,8 +12,13 @@
               </div>
             </li>
           </ul>
-          <span class="d-flex justify-content-end">Total {{ todoList.length }} items</span>
-          <span class="d-flex justify-content-end">Completed: {{ itemCount() }} items</span>
+          <span v-show="filteredTodos.length > 0" class="d-flex justify-content-end">Total: {{ filteredTodos.length }} items</span>
+          <span v-show="itemCount() > 0" class="d-flex justify-content-end">Completed: {{ itemCount() }} items</span>
+          <div class="d-flex justify-content-center align-items-center" :style="{ gap: '10px' }">
+            <button @click="setFilterType('')">All items</button>
+            <button @click="setFilterType('completed')">Completed items</button>
+            <button @click="setFilterType('uncompleted')">Uncompleted items</button>
+          </div>
     </div>
   </div>
 </template>
@@ -21,6 +26,7 @@
 export default {
   data() {
     return {
+      type: '',
       todoList: [
         {id: 1, text: "Learn Vue3.js", completed: false},
         {id: 2, text: "Learn React", completed: true},
@@ -30,7 +36,25 @@ export default {
       ]
     }
   },
+  computed: {
+    filteredTodos() {
+      return this.todoList.filter(todo => {
+        switch(this.type) {
+          case 'completed':
+            return todo.completed;
+          case 'uncompleted':
+            return !todo.completed;
+          default:
+            return true;
+        }
+      })
+    }
+  },
   methods: {
+    addTodo(todoText) {
+      this.todoList.push({ id: this._uid, text: todoText, completed: false});
+      this.todoText = null;
+    },
     isDone(item) {
       item.completed = !item.completed;
     },
@@ -39,13 +63,16 @@ export default {
     },
     itemCount() {
       let counter = 0;
-      for(let i = 0; i < this.todoList.length; i++) {
-        if(this.todoList[i].completed == true)
+      for(let i = 0; i < this.filteredTodos.length; i++) {
+        if(this.filteredTodos[i].completed == true)
           counter++;
       }
 
       return counter;
     },
+    setFilterType(type) {
+      this.type = type;
+    }
   }
 }
 </script>
